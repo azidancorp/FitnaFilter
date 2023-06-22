d6cce4739dc2d049aac63a6dbcccaf8837ae984e
yois615 <ynadiv@corpit.xyz>
2023-06-21 20:10:54 -0400

    Use HSV and YCrCb for more accurate detection
    This is an initial commit and the formula needs some
    more tweaking.

--- src/js/content/domManipulation.js
+++ src/js/content/domManipulation.js
@@ -584,14 +584,58 @@ function filterSkinColor(imgElement, uuid, canvas) {
         const bIndex = i + 2;
         const aIndex = i + 3;
 
-        const r = rgbaArray[rIndex];
-        const g = rgbaArray[gIndex];
-        const b = rgbaArray[bIndex];
+        r = rgbaArray[rIndex];
+        g = rgbaArray[gIndex];
+        b = rgbaArray[bIndex];
+
+        //Djamila Dahmani, Mehdi Cheref, Slimane Larabi, Zero-sum game theory model for segmenting skin regions
+        //Image and Vision Computing, Volume 99, 2020, 103925,ISSN 0262-8856, https://doi.org/10.1016/j.imavis.2020.103925.
+
+        //Convert to YCbCr
+        const y = (0.299 * r) + (0.587 * g) + (0.114 * b);
+        const cb = 128 + (-0.169 * r) + (-0.331 * g) + (0.5 * b);
+        const cr = 128 + (0.5 * r) + (-0.419 * g) + (-0.081 * b);
+
+        // Convert to HSV
+
+        let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+        rabs = r / 255;
+        gabs = g / 255;
+        babs = b / 255;
+        v = Math.max(rabs, gabs, babs),
+        diff = v - Math.min(rabs, gabs, babs);
+        diffc = c => (v - c) / 6 / diff + 1 / 2;
+        percentRoundFn = num => Math.round(num * 100) / 100;
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(rabs);
+            gg = diffc(gabs);
+            bb = diffc(babs);
+
+            if (rabs === v) {
+                h = bb - gg;
+            } else if (gabs === v) {
+                h = (1 / 3) + rr - bb;
+            } else if (babs === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            }else if (h > 1) {
+                h -= 1;
+            }
+        }
+
+        h = Math.round(h * 360),
+        s = percentRoundFn(s * 100),
+        v = percentRoundFn(v * 100)
 
         if (
-            (r > 95 && g > 40 && b > 20) &&
-            (Math.max(r, g, b) - Math.min(r, g, b) > 15) &&
-            (Math.abs(r - g) > 15 && r > g && r > b)
+            (h >= 0) && (h <= 32) && (s >= 15) && (s <= 170) && (v >= 0) && (v <= 255)
+            &&
+            (cb >= 85) && (cb <= 135) && (cr >= 135) && (cr < 180)
         ) {
             rgbaArray[rIndex] = 127;
             rgbaArray[gIndex] = 127;
